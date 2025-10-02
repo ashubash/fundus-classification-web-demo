3262607bbf4dabacfc1a475ea1686e7a45512089
Ashutosh Labh <145891404+ashubash@users.noreply.github.com>
2025-10-02 06:46:13 +0000

    editing index.jsx to import style.css and remove index.css

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './style.css';
 
 // --- ADD THIS CODE ---
 // Register the service worker for model caching
